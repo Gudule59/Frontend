6e6bf8593a70c83d44cf7a932face2244f1f844a
Fabien <locwallers@gmail.com>
2024-05-17 11:04:42 +0200

    banner / footer

---
+++ src/components/banners/banner.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function banner() {
+  return (
+    <nav>
+      <h1>Fabien CARLIEZ</h1>
+      <ul>
+        <li>
+          <a href="#intro">Présentation</a>
+        </li>
+        <li>
+          <a href="#projets">Projets</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
