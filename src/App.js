ca3f9fb26024d32294b5fdc30b156211db590ceb
Fabien <locwallers@gmail.com>
2024-05-17 16:53:01 +0200

    Ajout des competences

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import Banner from "./components/banners/banner.js";
 import Footer from './components/footer/footer.js';
 import Presentation from './components/presentation/presentation.js';
+import Outils from './components/outils/outils.js';
 
 function App() {
   return (
     <>
       <Banner />
       <Presentation />
+      <Outils />
       <Footer />
     </>
   );
---
+++ src/data/competence.jsx
@@ -0,0 +1,45 @@
+import iconeHTML from '../images/outils/icone_html.svg'
+import iconeCSS from '../images/outils/icone_css.svg';
+import iconeGithub from '../images/outils/icone_github.svg';
+import iconeJavaScript from '../images/outils/icone_javascript.svg';
+import iconeReact from '../images/outils/icone_react.svg';
+import iconeMongo from '../images/outils/icone_mongo.jpg';
+import iconeSass from '../images/outils/icone_sass.svg';
+
+
+const icone = [
+    {
+        "name": "HTML5",
+        "image": iconeHTML
+    },
+    {
+        "name": "CSS",
+        "image": iconeCSS
+    },
+    {
+        "name": "SASS",
+        "image": iconeSass
+    },
+    {
+        "name": "JAVASCRIPT",
+        "image": iconeJavaScript
+    },
+    {
+        "name": "REACT",
+        "image": iconeReact
+    },
+    {
+        "name": "MongoDB",
+        "image": iconeMongo
+    },
+    {
+        "name": "GITHUB",
+        "image": iconeGithub
+    },
+ 
+
+    
+  ]
+
+  export default icone
+  
